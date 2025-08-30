4e463e353d8d17fe8b93bfdb102dce8ebc940327
Vishal Pal <Vishalpal2613@gmail.com>
2025-08-30 21:52:53 +0530

    Header - Rendering Links and Buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header/Header';
 
 function App() {
   return (
     <div >
-     
+    <header>
+      <Header />
+    </header>
     </div>
   );
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,15 +1,33 @@
 import React from 'react'
-import { AppBar, Box, Toolbar } from '@mui/material';
+import { AppBar, Box, Tabs, Toolbar, Tab, Button, Link } from '@mui/material';
 import ApiIcon from '@mui/icons-material/Api';
 
+
+const links = ["Products", "Solutions", "Pricing", "Enterprise"];
 const Header = () => {
   return (
-    <AppBar sx={{bgcolor: "transparent", boxShadow: "0"}}>
-        <Toolbar>
-            <Box>
-                <ApiIcon sx={{color: "black"}} />
-            </Box>
-        </Toolbar>
+    <AppBar sx={{ bgcolor: "transparent", boxShadow: "0" }}>
+      <Toolbar>
+        <Box sx={{ display: "flex", width: "100%", alignItems: "center" }}>
+          <ApiIcon sx={{ color: "black" }} />
+          <Box>
+            <Tabs component={Link} sx={{textDecoration: "none"}}>
+              {links.map((link, index) => (
+                <Tab sx={{fontWeight: "bold", textDecoration: "none", ":hover": {
+                  textDecoration: 'underline',
+                  textUnderlineOffset: "5px",
+                }}} key={index} label={link} />
+              ))}
+            </Tabs>
+          </Box>
+          <Box display="flex" marginLeft={"auto"}>
+            <Button sx={{ marginRight: 2 }} variant='outlined'>Talk To Us</Button>
+            <Button sx={{ marginLeft: 3 }} variant='contained'>Try For Free</Button>
+
+
+          </Box>
+        </Box>
+      </Toolbar>
     </AppBar>
   )
 }
